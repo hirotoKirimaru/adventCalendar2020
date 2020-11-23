c9d4c87c4c9562008184cc051f9f034ee216c373
kirimaru <nainaistar@gmail.com>
2020-11-23 08:37:26 +0000

    [Add] action属性追加、edit追加

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -7,17 +7,25 @@ import com.example.demo.controller.dto.TodoDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         List<TodoDto> todos = List.of(TodoDto.builder().id(1).userId("kirimaru").build(),
-                TodoDto.builder().id(2).userId("kirimaru").build());
+                TodoDto.builder().id(2).userId("kirimaru").action("郵便局に行く").build());
 
         model.addAttribute("message", "きり丸さんこんにちは");
         model.addAttribute("todos", todos);
         return "index";
     }
+
+    @PostMapping("/")
+    public String edit(TodoDto model){
+        System.out.println(model);
+        return "redirect:/";
+    }
+
 
 }
--- src/main/java/com/example/demo/controller/dto/TodoDto.java
+++ src/main/java/com/example/demo/controller/dto/TodoDto.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoDto {
     int id;
     String userId;
+    String action;
 }
