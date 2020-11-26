9f353f2e5b98be8778973d53ed102c6f2218aaf2
nainaistar <nainaistar@gmail.com>
2020-11-26 18:27:48 +0900

    [Add] Controllerからファイルを読み込ませる

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
                 TodoDto.builder().id(2).userId("kirimaru").action("郵便局に行く").build());
 
         model.addAttribute("message", "きり丸さんこんにちは");
-        
+
         List<TodoDto> currentTodoList = todoRepository.findList("kirimaru");
         if (!currentTodoList.isEmpty()){
             todos = currentTodoList;
@@ -37,6 +37,7 @@ public class IndexController {
     public String edit(TodoDto model){
         System.out.println(model);
         System.out.println("修正ボタンクリック");
+        todoRepository.update(model);
         return "redirect:/";
     }
 
@@ -44,15 +45,16 @@ public class IndexController {
     public String add(TodoDto model){
         System.out.println(model);
         System.out.println("追加ボタンクリック");
-        
+
         todoRepository.insert(model);
         return "redirect:/";
     }
 
-        @PostMapping(value="/", params="delete")
+    @PostMapping(value="/", params="delete")
     public String delete(TodoDto model){
         System.out.println(model);
         System.out.println("削除ボタンクリック");
+        todoRepository.delete(model);
         return "redirect:/";
     }
 }
