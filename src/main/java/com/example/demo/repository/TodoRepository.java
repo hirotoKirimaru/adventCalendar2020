06e3dc034d1ef8447e2bf94311849c99dedf629f
nainaistar <nainaistar@gmail.com>
2020-11-26 18:19:13 +0900

    [Add] updateのメソッドを用意する

--- src/main/java/com/example/demo/repository/TodoRepository.java
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -7,13 +7,19 @@ import com.example.demo.controller.dto.TodoDto;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface TodoRepository {
-    
+
     @Insert("INSERT INTO todos (id, user_id, action) VALUES (#{id}, #{userId}, #{action})")
     public void insert(TodoDto todo);
 
+    @Update("UPDATE todos SET id=#{id}, user_id=#{userId}, action=#{action} where id = #{id} AND user_id = #{userId} ")
+    public void update(TodoDto todo);
+
     @Select("SELECT * FROM todos WHERE user_id = #{userId}")
     public List<TodoDto> findList(String userId);
+
+
 }
--- src/test/java/com/example/demo/repository/TodoRepositoryTests.java
+++ src/test/java/com/example/demo/repository/TodoRepositoryTests.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.controller.dto.TodoDto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,39 +14,57 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @MybatisTest
 public class TodoRepositoryTests {
-    @Autowired
-    TodoRepository todoRepository;
+  @Autowired
+  TodoRepository todoRepository;
 
-    // @Nested
-    // class Insert {
-
-    // }
+  @Nested
+  class Insert {
     @DisplayName("insertした結果をfindする")
     @Test
-    public void test_01(){
-        TodoDto todoDto = TodoDto.builder().id(1).userId("kirimaru").action("豆腐買う").build();
+    public void test_01() {
+      TodoDto todoDto = TodoDto.builder().id(1).userId("kirimaru").action("豆腐買う").build();
 
-        todoRepository.insert(todoDto);
+      todoRepository.insert(todoDto);
 
-        final List<TodoDto> kirimaru = todoRepository.findList("kirimaru");
-        assertThat(kirimaru).isEqualTo(
-            List.of(todoDto)
-        );
+      final List<TodoDto> kirimaru = todoRepository.findList("kirimaru");
+      assertThat(kirimaru).isEqualTo(
+          List.of(todoDto)
+      );
     }
 
     @DisplayName("insertした結果をfindする。複数。")
     @Test
-    public void test_02(){
-        TodoDto todoDto = TodoDto.builder().id(1).userId("kirimaru").action("豆腐買う").build();
-        TodoDto todoDto2 = TodoDto.builder().id(2).userId("kirimaru").action("郵便局行く").build();
+    public void test_02() {
+      TodoDto todoDto = TodoDto.builder().id(1).userId("kirimaru").action("豆腐買う").build();
+      TodoDto todoDto2 = TodoDto.builder().id(2).userId("kirimaru").action("郵便局行く").build();
 
-        todoRepository.insert(todoDto);
-        todoRepository.insert(todoDto2);
+      todoRepository.insert(todoDto);
+      todoRepository.insert(todoDto2);
 
-        final List<TodoDto> kirimaru = todoRepository.findList("kirimaru");
-        assertThat(kirimaru).isEqualTo(
-            List.of(todoDto,todoDto2)
-        );
+      final List<TodoDto> kirimaru = todoRepository.findList("kirimaru");
+      assertThat(kirimaru).isEqualTo(
+          List.of(todoDto, todoDto2)
+      );
     }
+  }
 
+  @Nested
+  class Update {
+    @DisplayName("updateを行う")
+    @Test
+    public void test_01() {
+      // GIVEN
+      TodoDto todoDto = TodoDto.builder().id(1).userId("kirimaru").action("豆腐買う").build();
+      todoRepository.insert(todoDto);
+
+      // WHEN
+      TodoDto expected = TodoDto.builder().id(1).userId("kirimaru").action("納豆買う").build();
+      todoRepository.update(expected);
+
+      final List<TodoDto> kirimaru = todoRepository.findList("kirimaru");
+      assertThat(kirimaru).isEqualTo(
+          List.of(expected)
+      );
+    }
+  }
 }
