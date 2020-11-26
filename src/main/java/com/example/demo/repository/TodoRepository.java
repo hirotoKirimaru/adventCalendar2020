042f1ea43fd3c91d17397ea04f919b920764fac6
nainaistar <nainaistar@gmail.com>
2020-11-26 18:24:56 +0900

    [Add] publicを削除

--- src/main/java/com/example/demo/repository/TodoRepository.java
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -9,15 +9,15 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface TodoRepository {
 
-    @Insert("INSERT INTO todos (id, user_id, action) VALUES (#{id}, #{userId}, #{action})")
-    public void insert(TodoDto todo);
+  @Insert("INSERT INTO todos (id, user_id, action) VALUES (#{id}, #{userId}, #{action})")
+  void insert(TodoDto todo);
 
-    @Update("UPDATE todos SET id=#{id}, user_id=#{userId}, action=#{action} where id = #{id} AND user_id = #{userId} ")
-    public void update(TodoDto todo);
+  @Update("UPDATE todos SET id=#{id}, user_id=#{userId}, action=#{action} where id = #{id} AND user_id = #{userId} ")
+  void update(TodoDto todo);
 
-    @Select("SELECT * FROM todos WHERE user_id = #{userId}")
-    public List<TodoDto> findList(String userId);
+  @Select("SELECT * FROM todos WHERE user_id = #{userId}")
+  List<TodoDto> findList(String userId);
 
-    @Delete("DELETE FROM todos WHERE id=#{id}AND user_id = #{userId}")
-    void delete(TodoDto todoDto);
+  @Delete("DELETE FROM todos WHERE id=#{id}AND user_id = #{userId}")
+  void delete(TodoDto todoDto);
 }
