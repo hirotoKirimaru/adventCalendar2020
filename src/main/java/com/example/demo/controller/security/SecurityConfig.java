c12bed29bcd25cb81fe34e447f0f0750166ba4fe
nainaistar <nainaistar@gmail.com>
2020-11-28 10:21:49 +0900

    [Add] 一旦デフォルト機能で作成する

--- src/main/java/com/example/demo/controller/security/SecurityConfig.java
+++ src/main/java/com/example/demo/controller/security/SecurityConfig.java
@@ -1,40 +1,24 @@
 package com.example.demo.controller.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-private final UserDetailsService userDetailsService;
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
-
-  @Override
-  public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-  }
-
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
 
     http.formLogin();
 
-    http.logout()
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-        .deleteCookies("SESSION", "JSESSIONID")
-        .invalidateHttpSession(true).permitAll();
+//    http.logout()
+//        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//        .deleteCookies("SESSION", "JSESSIONID")
+//        .invalidateHttpSession(true)
+//        .permitAll();
 
     http.httpBasic();
   }
