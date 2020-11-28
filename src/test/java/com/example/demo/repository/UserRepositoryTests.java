4e1f381fedb37315e6d48da35951843b1c2c486e
nainaistar <nainaistar@gmail.com>
2020-11-28 15:03:49 +0900

    [Add] admin以外のデータを設定

--- src/test/java/com/example/demo/repository/UserRepositoryTests.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTests.java
@@ -26,10 +26,10 @@ class UserRepositoryTests {
 
   @Test
   public void test_02(){
-    jdbcTemplate.execute("INSERT INTO LOGIN_USER VALUES ('admin', 'password')");
+    jdbcTemplate.execute("INSERT INTO LOGIN_USER VALUES ('test', 'password')");
 
-    final UserDto expected = UserDto.builder().userId("admin").password("password").build();
-    final UserDto user = userRepository.findByUserName("admin");
+    final UserDto expected = UserDto.builder().userId("test").password("password").build();
+    final UserDto user = userRepository.findByUserName("test");
 
     assertThat(user).isEqualTo(expected);
 
