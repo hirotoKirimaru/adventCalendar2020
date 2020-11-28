123399ce692ad76f4e4ace71b8f9a8ee633ac722
nainaistar <nainaistar@gmail.com>
2020-11-28 14:57:27 +0900

    [Add] ログインユーザのデータを取得できるようにする

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.List;
 
 import com.example.demo.controller.dto.TodoDto;
+import com.example.demo.controller.security.AuthTargetUser;
 import com.example.demo.repository.TodoRepository;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +15,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final TodoRepository todoRepository;
+    private final HttpSession session;
 
     @GetMapping("/")
     public String index(Model model) {
-        List<TodoDto> todos = todoRepository.findList("kirimaru");
+        SecurityContext securityContext = (SecurityContext)session.getAttribute("SPRING_SECURITY_CONTEXT");
+        Authentication authentication = securityContext.getAuthentication();
+        AuthTargetUser principal = (AuthTargetUser) authentication.getPrincipal();
+
+        List<TodoDto> todos = todoRepository.findList(principal.getUsername());
 
         model.addAttribute("todos", todos);
         return "index";
