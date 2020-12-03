895ac0b1a1aa761074185483686a2855b3c516c1
nainaistar <nainaistar@gmail.com>
2020-12-03 23:33:50 +0900

    [Add] RestApiのテスト追加

--- src/main/java/com/example/demo/controller/restapi/TodoRestController.java
+++ src/main/java/com/example/demo/controller/restapi/TodoRestController.java
@@ -1,14 +1,28 @@
 package com.example.demo.controller.restapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.controller.dto.TodoDto;
+import com.example.demo.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/todos")
 public class TodoRestController {
-  @GetMapping("")
-  public String get()  {
-    return "hogehoge";
+  private final TodoRepository todoRepository;
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<List<TodoDto>> get(@PathVariable String userId)  {
+
+    final List<TodoDto> list = todoRepository.findList(userId);
+
+    if (list.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    } else {
+      return ResponseEntity.ok(list);
+    }
   }
 }
--- src/test/java/com/example/demo/controller/restapi/TodoRestControllerTests.java
+++ src/test/java/com/example/demo/controller/restapi/TodoRestControllerTests.java
@@ -1,26 +1,69 @@
 package com.example.demo.controller.restapi;
 
+import com.example.demo.controller.dto.TodoDto;
+import com.example.demo.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TodoRestController.class)
+@WithMockUser(value = "spring")
 public class TodoRestControllerTests {
   @Autowired
   private MockMvc mockMvc;
   private final String rootUrl = "/v1/todos";
 
-  @WithMockUser(value = "spring")
+  @MockBean
+  private TodoRepository todoRepository;
+
   @Test
   void success() throws Exception {
-    this.mockMvc.perform(get(rootUrl))
+    // GIVEN
+    when(todoRepository.findList("spring"))
+        .thenReturn(List.of(
+            TodoDto.builder().id(1).userId("spring").action("醤油").build(),
+            TodoDto.builder().id(2).userId("spring").action("豆腐").build()
+
+        ));
+
+    // Language=json
+    String expected = "[" +
+        "{\n" +
+        "  \"id\": 1,\n" +
+        "  \"userId\": \"spring\",\n" +
+        "  \"action\": \"醤油\"\n" +
+        "},\n" +
+        "  " +
+        "{\n" +
+        "    \"id\": 2,\n" +
+        "    \"userId\": \"spring\",\n" +
+        "    \"action\": \"豆腐\"\n" +
+        "  " +
+        "}\n" +
+        "]";
+
+    this.mockMvc.perform(get(rootUrl + "/spring"))
         .andExpect(status().isOk())
-        .andExpect(content().string("hogehoge"));
+        .andExpect(content().json(expected));
+  }
+
+  @Test
+  void _404_not_found() throws Exception {
+    when(todoRepository.findList("spring"))
+        .thenReturn(List.of());
+
+    this.mockMvc.perform(get(rootUrl + "/spring"))
+        .andExpect(status().isNotFound())
+    ;
   }
 }
