d0b4e8db86c8f20570f8e0a47bbf1b3ec679035a
nainaistar <nainaistar@gmail.com>
2020-12-13 23:01:34 +0900

    [Add] ローカルでAPサーバを建てるようにする

--- src/test/java/com/example/demo/external/zipCloud/ZipCloudClientImplTests.java
+++ src/test/java/com/example/demo/external/zipCloud/ZipCloudClientImplTests.java
@@ -4,23 +4,31 @@ import com.example.demo.external.dto.ZipAddressDto;
 import com.example.demo.external.dto.ZipCloudDto;
 import com.example.demo.external.operation.RestOperationFactory;
 import com.example.demo.external.operation.RestTemplateInterceptor;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.web.client.RestOperations;
+import ru.lanwen.wiremock.ext.WiremockResolver;
 
 import java.util.List;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringJUnitConfig(classes = {
     RestTemplateAutoConfiguration.class
 })
+@ExtendWith(WiremockResolver.class)
 class ZipCloudClientImplTests {
 
   ZipCloudClient target;
@@ -28,6 +36,111 @@ class ZipCloudClientImplTests {
   @Autowired
   private RestTemplateBuilder restTemplateBuilder;
 
+  private WireMockServer server;
+
+  ZipCloudClientImplTests(@WiremockResolver.Wiremock WireMockServer server) {
+    this.server = server;
+  }
+
+  @BeforeEach
+  void setup() {
+    ZipCloudClientProperties properties = new ZipCloudClientProperties();
+    properties.setSchema("http");
+    properties.setHost("localhost");
+    properties.setPort(server.port());
+    properties.setPath("/api/search");
+
+    RestOperationFactory restOperationFactory = new RestOperationFactory(restTemplateBuilder, new RestTemplateInterceptor());
+    RestOperations restOperations = restOperationFactory.createRestOperations(properties);
+
+    target = new ZipCloudClientImpl(properties, restOperations);
+  }
+
+  @Test
+  void test_01() {
+    // GIVEN
+    // Language=JSON
+    String responseBody =
+        """
+            {
+              "message":null,
+              "results": [{
+                "address1":"神奈川県",
+                "address2" : "厚木市",
+                "address3" : "中町",
+                "kana1" : "ｶﾅｶﾞﾜｹﾝ",
+                "kana2" : "ｱﾂｷﾞｼ",
+                "kana3" : "ﾅｶﾁｮｳ",
+                "prefcode" : "14",
+                "zipcode" : "2430018"
+              }],
+              "status": 200
+            }""";
+
+    server.stubFor(
+        get("/api/search?zipcode=2430018").willReturn(
+            aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "text/plain")
+                .withBody(responseBody)
+        )
+    );
+
+
+    ZipCloudDto expected = ZipCloudDto.builder()
+        .message(null)
+        .results(List.of(
+            ZipAddressDto.builder()
+                .address1("神奈川県")
+                .address2("厚木市")
+                .address3("中町")
+                .kana1("ｶﾅｶﾞﾜｹﾝ")
+                .kana2("ｱﾂｷﾞｼ")
+                .kana3("ﾅｶﾁｮｳ")
+                .prefcode("14")
+                .zipcode("2430018")
+                .build()
+        ))
+        .status("200")
+        .build();
+
+    ZipCloudDto actual = target.getAddressByZipcode("2430018");
+
+    assertThat(actual).isEqualTo(expected);
+
+    server.verify(1, getRequestedFor(urlEqualTo("/api/search?zipcode=2430018")));
+  }
+
+  @Test
+  void test_02() {
+    // Language=JSON
+    String responseBody = """
+        {
+          "message": "パラメータ「郵便番号」の桁数が不正です。",
+          "status": 400
+        }""";
+
+    server.stubFor(
+        get("/api/search?zipcode=XXX").willReturn(
+            aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "text/plain")
+                .withBody(responseBody)
+        )
+    );
+
+
+    ZipCloudDto expected = ZipCloudDto.builder()
+        .message("パラメータ「郵便番号」の桁数が不正です。")
+        .status("400")
+        .build();
+
+    ZipCloudDto actual = target.getAddressByZipcode("XXX");
+
+    assertThat(actual).isEqualTo(expected);
+    server.verify(1, getRequestedFor(urlEqualTo("/api/search?zipcode=XXX")));
+
+  }
 
   @Disabled("毎回実行すると迷惑なので、基本はDisabledにする")
   @Nested
