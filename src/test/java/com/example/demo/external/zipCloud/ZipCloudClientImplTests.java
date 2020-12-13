67074ee34d03072d57459d11eb02e8cd2c960927
nainaistar <nainaistar@gmail.com>
2020-12-13 22:17:17 +0900

    [Add] Nestしてテストが動かないようにする

--- src/test/java/com/example/demo/external/zipCloud/ZipCloudClientImplTests.java
+++ src/test/java/com/example/demo/external/zipCloud/ZipCloudClientImplTests.java
@@ -5,6 +5,8 @@ import com.example.demo.external.dto.ZipCloudDto;
 import com.example.demo.external.operation.RestOperationFactory;
 import com.example.demo.external.operation.RestTemplateInterceptor;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
@@ -26,54 +28,58 @@ class ZipCloudClientImplTests {
   @Autowired
   private RestTemplateBuilder restTemplateBuilder;
 
-  @BeforeEach
-  void setup(){
-    ZipCloudClientProperties properties = new ZipCloudClientProperties();
-    properties.setSchema("https");
-    properties.setHost("zipcloud.ibsnet.co.jp");
-    properties.setPort(0);
-    properties.setPath("/api/search");
 
-    RestOperationFactory restOperationFactory = new RestOperationFactory(restTemplateBuilder, new RestTemplateInterceptor());
-    RestOperations restOperations = restOperationFactory.createRestOperations(properties);
+  @Disabled("毎回実行すると迷惑なので、基本はDisabledにする")
+  @Nested
+  class Production {
+    @BeforeEach
+    void setup() {
+      ZipCloudClientProperties properties = new ZipCloudClientProperties();
+      properties.setSchema("https");
+      properties.setHost("zipcloud.ibsnet.co.jp");
+      properties.setPort(0);
+      properties.setPath("/api/search");
 
-    target = new ZipCloudClientImpl(properties, restOperations);
+      RestOperationFactory restOperationFactory = new RestOperationFactory(restTemplateBuilder, new RestTemplateInterceptor());
+      RestOperations restOperations = restOperationFactory.createRestOperations(properties);
+
+      target = new ZipCloudClientImpl(properties, restOperations);
+    }
+
+    @Test
+    void test_01() {
+      ZipCloudDto expected = ZipCloudDto.builder()
+          .message(null)
+          .results(List.of(
+              ZipAddressDto.builder()
+                  .address1("神奈川県")
+                  .address2("厚木市")
+                  .address3("中町")
+                  .kana1("ｶﾅｶﾞﾜｹﾝ")
+                  .kana2("ｱﾂｷﾞｼ")
+                  .kana3("ﾅｶﾁｮｳ")
+                  .prefcode("14")
+                  .zipcode("2430018")
+                  .build()
+          ))
+          .status("200")
+          .build();
+
+      ZipCloudDto actual = target.getAddressByZipcode("2430018");
+
+      assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void test_02() {
+      ZipCloudDto expected = ZipCloudDto.builder()
+          .message("パラメータ「郵便番号」の桁数が不正です。")
+          .status("400")
+          .build();
+
+      ZipCloudDto actual = target.getAddressByZipcode("XXX");
+
+      assertThat(actual).isEqualTo(expected);
+    }
   }
-
-  @Test
-  void test_01(){
-    ZipCloudDto expected = ZipCloudDto.builder()
-        .message(null)
-        .results(List.of(
-            ZipAddressDto.builder()
-                .address1("神奈川県")
-                .address2("厚木市")
-                .address3("中町")
-                .kana1("ｶﾅｶﾞﾜｹﾝ")
-                .kana2("ｱﾂｷﾞｼ")
-                .kana3("ﾅｶﾁｮｳ")
-                .prefcode("14")
-                .zipcode("2430018")
-                .build()
-        ))
-        .status("200")
-        .build();
-
-    ZipCloudDto actual = target.getAddressByZipcode("2430018");
-
-    assertThat(actual).isEqualTo(expected);
-  }
-
-  @Test
-  void test_02(){
-    ZipCloudDto expected = ZipCloudDto.builder()
-        .message("パラメータ「郵便番号」の桁数が不正です。")
-        .status("400")
-        .build();
-
-    ZipCloudDto actual = target.getAddressByZipcode("XXX");
-
-    assertThat(actual).isEqualTo(expected);
-  }
-
 }
