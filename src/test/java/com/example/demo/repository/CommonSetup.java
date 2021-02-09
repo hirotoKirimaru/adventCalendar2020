67e83384184afd293a0196305b12ed5389f5194a
nainaistar <nainaistar@gmail.com>
2021-02-09 16:32:37 +0900

    [Add] いい感じにマッピングするメソッド

--- src/test/java/com/example/demo/repository/CommonSetup.java
+++ src/test/java/com/example/demo/repository/CommonSetup.java
@@ -2,13 +2,18 @@ package com.example.demo.repository;
 
 import com.example.demo.repository.dto.DummyDto;
 import com.example.demo.repository.dto.UserDto;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import javax.sql.DataSource;
+import java.io.Serializable;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @MybatisTest
 public class CommonSetup {
@@ -50,6 +55,30 @@ public class CommonSetup {
       this.simpleJdbcInsert.execute(param);
     }
 
+  }
+
+  public Map<String, Object> toMap(Serializable entity) {
+    HashMap<String, Object> rtn = new HashMap<>();
+    var source = new BeanPropertySqlParameterSource(entity);
+
+    for (String readablePropertyName : source.getReadablePropertyNames()) {
+      // クラス名は不要
+      if (List.of("class", "blank", "bytes", "empty").contains(readablePropertyName)) {
+        continue;
+      }
+
+      var snakeName = new PropertyNamingStrategy.SnakeCaseStrategy().translate(readablePropertyName);
+      Object object = source.getValue(readablePropertyName);
+
+      if (object instanceof Serializable) {
+        var primaryKeyMap = toMap((Serializable) object);
+        rtn.putAll(primaryKeyMap);
+      }
+
+      rtn.put(snakeName, object);
+    }
+
+    return rtn;
   }
 
 }
---
+++ src/test/java/com/example/demo/repository/CommonSetupTests.java
@@ -0,0 +1,56 @@
+package com.example.demo.repository;
+
+import com.example.demo.repository.dto.DummyDto;
+import com.example.demo.repository.dto.DummyIdDto;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonSetupTests {
+  CommonSetup target = new CommonSetup();
+
+  @DisplayName(
+      """
+            ・Mapのkeyはキャメルケースで設定すること。
+            ・Mapのvalueはエンティティを利用すること。
+            ・主キー項目が複合主キーの場合でもマッピングできること
+            ・その他の項目はマッピングされないこと            
+          """
+  )
+  @Test
+  void test_01() {
+    String idFirst = "111";
+    String idSecond = "222";
+    String fieldFirst = "1";
+    String fieldSecond = "2";
+    String fieldThird = "3";
+
+    DummyIdDto dummyIdDto = DummyIdDto.builder()
+        .idFirst(idFirst)
+        .idSecond(idSecond)
+        .build();
+
+    DummyDto dummyDto = DummyDto.builder()
+        .id(dummyIdDto)
+        .fieldFirst(fieldFirst)
+        .fieldSecond(fieldSecond)
+        .fieldThird(fieldThird)
+        .build();
+
+    Map<String, Object> actual = target.toMap(dummyDto);
+
+    SoftAssertions softly = new SoftAssertions();
+    softly.assertThat(actual).hasSize(6);
+    softly.assertThat(actual).containsEntry("id_first", idFirst);
+    softly.assertThat(actual).containsEntry("id_second", idSecond);
+    softly.assertThat(actual).containsEntry("field_first", fieldFirst);
+    softly.assertThat(actual).containsEntry("field_second", fieldSecond);
+    softly.assertThat(actual).containsEntry("field_third", fieldThird);
+    softly.assertAll();
+  }
+}
